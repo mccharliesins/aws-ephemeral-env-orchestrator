f37311fb1cfd5b39d2bf99feba3a8686b36235fc
mccharliesins <vkyd1909@gmail.com>
2024-12-23 21:21:19 -0500

    feat: implement blueprint s3 bucket

--- infra-templates/app-stack.ts
+++ infra-templates/app-stack.ts
@@ -1,1 +1,23 @@
-// blueprint
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+// blueprint stack logic
+// duplicatable for each ephemeral environment
+export class AppStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // sample resource for demonstration
+        new s3.Bucket(this, 'EphemeralBucket', {
+            versioned: false,
+            removalPolicy: cdk.RemovalPolicy.DESTROY, // ensure easy cleanup
+            autoDeleteObjects: true, // allow bucket deletion when not empty
+        });
+
+        // output stack name for user return
+        new cdk.CfnOutput(this, 'StackName', {
+            value: this.stackName,
+        });
+    }
+}
